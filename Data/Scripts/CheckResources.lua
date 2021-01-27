64020184d3784a7af7f20dfd8df1ee9c2322fda5
Lain818 <info@wtp-web.com>
2021-01-27 16:36:13 +0100

    Update CheckResources.lua

--- Data/Scripts/CheckResources.lua
+++ Data/Scripts/CheckResources.lua
@@ -6,12 +6,4 @@ Task.Wait()
 for k, v in pairs(resourceTable) do
     print("Resource ["..k.."]: " .. v)
 end
-function PlayerSkill()
-    return _G["Meta.Player.Skills"]
-end
 
-function Tick()
-   --local xp = PlayerSkill().GetSkillXp(player, PlayerSkill().DEX)
-    --print(xp)
-    Task.Wait(1)
-end
