781620c6ab2e6b889f892e3773837dcc2a375ef4
Lain818 <info@wtp-web.com>
2021-01-30 14:33:03 +0100

    1

--- Data/Scripts/EventsRewardsForResourcesServer.lua
+++ Data/Scripts/EventsRewardsForResourcesServer.lua
@@ -12,6 +12,9 @@ function Madelvl1Timber(player)
     local Wood = ItemDatabase:GetItemFromName("Wood lvl 1")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    --if not player.serverUserData.statSheet then return end
+    player:AddResource("TimberingExperience", 9)
+
 end
 Events.ConnectForPlayer("Madelvl1Timber", Madelvl1Timber)
 
@@ -25,6 +28,7 @@ function Madelvl2Timber(player)
     local Wood = ItemDatabase:GetItemFromName("Wood lvl 2")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("TimberingExperience", 13)
 end
 Events.ConnectForPlayer("Madelvl2Timber", Madelvl2Timber)
 
@@ -38,6 +42,7 @@ function Madelvl3Timber(player)
     local Wood = ItemDatabase:GetItemFromName("Wood lvl 3")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("TimberingExperience", 16)
 end
 Events.ConnectForPlayer("Madelvl3Timber", Madelvl3Timber)
 
@@ -51,6 +56,7 @@ function Madelvl4Timber(player)
     local Wood = ItemDatabase:GetItemFromName("Wood lvl 4")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("TimberingExperience", 21)
 end
 Events.ConnectForPlayer("Madelvl4Timber", Madelvl4Timber)
 
@@ -64,6 +70,7 @@ function Madelvl5Timber(player)
     local Wood = ItemDatabase:GetItemFromName("Wood lvl 5")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("TimberingExperience", 25)
 end
 Events.ConnectForPlayer("Madelvl5Timber", Madelvl5Timber)
 
@@ -135,6 +142,7 @@ function Madelvl1Metal(player)
     local Wood = ItemDatabase:GetItemFromName("Ore lvl 1")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("MiningExperience", 9)
 end
 Events.ConnectForPlayer("Madelvl1Metal", Madelvl1Metal)
 
@@ -148,6 +156,7 @@ function Madelvl2Metal(player)
     local Wood = ItemDatabase:GetItemFromName("Ore lvl 2")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("MiningExperience", 13)
 end
 Events.ConnectForPlayer("Madelvl2Metal", Madelvl2Metal)
 
@@ -161,6 +170,7 @@ function Madelvl3Metal(player)
     local Wood = ItemDatabase:GetItemFromName("Ore lvl 3")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("MiningExperience", 16)
 end
 Events.ConnectForPlayer("Madelvl3Metal", Madelvl3Metal)
 
@@ -174,6 +184,7 @@ function Madelvl4Metal(player)
     local Wood = ItemDatabase:GetItemFromName("Ore lvl 4")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("MiningExperience", 21)
 end
 Events.ConnectForPlayer("Madelvl4Metal", Madelvl4Metal)
 
@@ -187,6 +198,7 @@ function Madelvl5Metal(player)
     local Wood = ItemDatabase:GetItemFromName("Ore lvl 5")
     playersInventory:AddItem(Timber, 1)
     playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("MiningExperience", 25)
 end
 Events.ConnectForPlayer("Madelvl5Metal", Madelvl5Metal)
 
@@ -244,5 +256,129 @@ end
 Events.ConnectForPlayer("Tradelvl4Metal", Tradelvl4Metal)
 
 
+
+
+function Madelvl1Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 1")
+    local Wood = ItemDatabase:GetItemFromName("Cotton")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("PlantKnowledge", 9)
+end
+Events.ConnectForPlayer("Madelvl1Textile", Madelvl1Textile)
+
+function Madelvl2Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 2")
+    local Wood = ItemDatabase:GetItemFromName("Linen")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("PlantKnowledge", 13)
+end
+Events.ConnectForPlayer("Madelvl2Textile", Madelvl2Textile)
+
+function Madelvl3Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 3")
+    local Wood = ItemDatabase:GetItemFromName("Jute")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("PlantKnowledge", 16)
+end
+Events.ConnectForPlayer("Madelvl3Textile", Madelvl3Textile)
+
+function Madelvl4Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 4")
+    local Wood = ItemDatabase:GetItemFromName("Hemp")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("PlantKnowledge", 21)
+end
+Events.ConnectForPlayer("Madelvl4Textile", Madelvl4Textile)
+
+function Madelvl5Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 5")
+    local Wood = ItemDatabase:GetItemFromName("Ramie")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+    player:AddResource("PlantKnowledge", 21)
+end
+Events.ConnectForPlayer("Madelvl5Textile", Madelvl5Textile)
+
+
+function Tradelvl1Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 2")
+    local Wood = ItemDatabase:GetItemFromName("Textile lvl 1")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+end
+Events.ConnectForPlayer("Tradelvl1Textile", Tradelvl1Textile)
+
+function Tradelvl2Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 3")
+    local Wood = ItemDatabase:GetItemFromName("Textile lvl 2")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+end
+Events.ConnectForPlayer("Tradelvl2Textile", Tradelvl2Textile)
+
+function Tradelvl3Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 4")
+    local Wood = ItemDatabase:GetItemFromName("Textile lvl 3")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+end
+Events.ConnectForPlayer("Tradelvl3Textile", Tradelvl3Textile)
+
+function Tradelvl4Textile(player)
+    Task.Wait(1)
+    while not player.serverUserData.inventory do Task.Wait() end
+    local playersInventory = player.serverUserData.inventory
+    playersInventory:WaitUntilLoaded()
+    local ItemDatabase = playersInventory.database
+    local Timber = ItemDatabase:GetItemFromName("Textile lvl 5")
+    local Wood = ItemDatabase:GetItemFromName("Textile lvl 4")
+    playersInventory:AddItem(Timber, 1)
+    playersInventory:RemoveItem(Wood, 3)
+end
+Events.ConnectForPlayer("Tradelvl4Textile", Tradelvl4Textile)
 
 
